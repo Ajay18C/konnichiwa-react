8adc0cb308f7ea4b769c37dbceeaed35d88122c6
Aj_ay_c <a.christopher@invokeinc.com>
2024-07-21 22:10:30 +0530

    solved swiggy issue

--- src/components/Body.js
+++ src/components/Body.js
@@ -7,13 +7,15 @@ const Body = () => {
   // Local State variable - Super powerful variable
   const [listOfResturant,setListOfResturant] = useState([]);
   const [searchText,setSearchText] = useState("");
+  const [filterListOfResturant,setFilterListOfResturant] = useState([]);
 
   const fetchData = async () => {
-    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.99740&lng=79.00110&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&is-seo-homepage-enable=true&page_type=DESKTOP_WEB_LISTING");
     const json = await data.json();
     console.log(json);
-    console.log(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
-    setListOfResturant(resObj);
+    console.log(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    setListOfResturant(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    setFilterListOfResturant(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
   }
   useEffect(()=>{
     fetchData();
@@ -34,7 +36,7 @@ const Body = () => {
         <div className="search">
           <input type="text" className="search-box" value={searchText} onChange={(e) => {setSearchText(e.target.value)}}/>
           <button onClick={() =>{
-            setListOfResturant(resObj.filter((res) => res.info.name.toLowerCase().includes(searchText.toLowerCase())))
+            setListOfResturant(filterListOfResturant.filter((res) => res.info.name.toLowerCase().includes(searchText.toLowerCase())))
           }}>Search</button>
         </div>
         <button
